4b1001641a1ef36d7e177821fbb870f742559e3d
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2023-07-14 13:12:18 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
+
+const LoginPage = () => {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+
+  return (
+    <div className="text-gray-300 flex flex-col items-center justify-center min-h-screen py-2 gap-4">
+      <h1 className="text-8xl ">Login</h1>
+      <br />
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+        className="text-black p-2 rounded focus:outline-none focus:border-gray-600"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="text"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+        className="text-black p-2 rounded focus:outline-none focus:border-gray-600"
+      />
+      <button
+        onClick={onLogin}
+        className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+      >
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+          Login
+        </span>
+      </button>
+      <Link href="/signup">Signup Page</Link>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,18 @@
+import mongoose, { connection } from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = await mongoose.connection;
+    connection.on("connected", () => {
+      console.log(`Mongoose connected to ${connection.name}`);
+    });
+    connection.on("error", (err) => {
+      console.log(`Mongoose error: ${err}`);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("something went wrong!");
+    console.log(error);
+  }
+}
