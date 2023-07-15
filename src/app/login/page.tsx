d69c2aff099856503821108e1e88aa384f55ce45
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2023-07-16 04:51:48 +0530

    created login route

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,45 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { Axios } from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const LoginPage = () => {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
-  const onLogin = async () => {};
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login Failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(true);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="text-gray-300 flex flex-col items-center justify-center min-h-screen py-2 gap-4">
-      <h1 className="text-8xl ">Login</h1>
+      <h1 className="text-8xl">{loading ? "Processing" : "Login"}</h1>
       <br />
       <label htmlFor="email">Email</label>
       <input
@@ -39,7 +64,7 @@ const LoginPage = () => {
         className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
       >
         <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Login
+          {buttonDisabled ? "Fill Details" : "Login"}
         </span>
       </button>
       <Link href="/signup">Signup Page</Link>
