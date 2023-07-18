0cc32c5e1e990804af1eb65d804e3fb351cad2e9
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2023-07-19 04:17:04 +0530

    created mailer and verification

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel.js";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 connect();
 
@@ -25,8 +26,13 @@ export async function POST(request: NextRequest) {
     const salt = await bcryptjs.genSalt(10);
     const hashedPassword = await bcryptjs.hash(password, salt);
     const newUser = new User({ username, email, password: hashedPassword });
+
     const savedUser = await newUser.save();
     console.log(savedUser);
+
+    //send verification email
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id });
+
     return NextResponse.json({
       message: "User created successfully",
       success: true,
